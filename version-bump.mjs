bf512523d77423a825206e5e7590eedc1b5e6358
eugenesvk <eugenesvk@users.noreply.github.com>
2026-02-27 16:16:33 +0700

    add version bump script

---
+++ version-bump.mjs
@@ -0,0 +1,16 @@
+// Auto add a new entry to `versions.json` for the current version of your theme: `$ npm run version`
+
+import { readFileSync, writeFileSync } from "fs";
+
+const targetVersion = process.env.npm_package_version;
+
+// read minAppVersion from manifest.json and bump version to target version
+let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+const { minAppVersion } = manifest;
+manifest.version = targetVersion;
+writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+
+// update versions.json with target version and minAppVersion from manifest.json
+let versions = JSON.parse(readFileSync("versions.json", "utf8"));
+versions[targetVersion] = minAppVersion;
+writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
